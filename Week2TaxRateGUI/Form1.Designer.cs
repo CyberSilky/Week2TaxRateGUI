86a6edda184d0199ece9ebad9770af4d1f7ad4fe
James Shoemaker <james.shoemaker99@gmail.com>
2020-10-27 13:40:41 -0500

    Add project files.

---
+++ Week2TaxRateGUI/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Week2TaxRateGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbTaxRate = new System.Windows.Forms.ComboBox();
+            this.lblTaxRate = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbTaxRate
+            // 
+            this.cmbTaxRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cmbTaxRate.FormattingEnabled = true;
+            this.cmbTaxRate.Items.AddRange(new object[] {
+            "5%",
+            "6%",
+            "7%",
+            "8%",
+            "9%"});
+            this.cmbTaxRate.Location = new System.Drawing.Point(284, 34);
+            this.cmbTaxRate.Name = "cmbTaxRate";
+            this.cmbTaxRate.Size = new System.Drawing.Size(121, 33);
+            this.cmbTaxRate.TabIndex = 0;
+            this.cmbTaxRate.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblTaxRate
+            // 
+            this.lblTaxRate.AutoSize = true;
+            this.lblTaxRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTaxRate.Location = new System.Drawing.Point(58, 36);
+            this.lblTaxRate.Name = "lblTaxRate";
+            this.lblTaxRate.Size = new System.Drawing.Size(221, 31);
+            this.lblTaxRate.TabIndex = 1;
+            this.lblTaxRate.Text = "Choose a tax rate: ";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblPrice.Location = new System.Drawing.Point(60, 104);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(155, 25);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "Enter Item Price:";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblQuantity.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblQuantity.Location = new System.Drawing.Point(62, 172);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(169, 25);
+            this.lblQuantity.TabIndex = 3;
+            this.lblQuantity.Text = "Enter the quantity:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPrice.Location = new System.Drawing.Point(284, 96);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(125, 38);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtQuantity.Location = new System.Drawing.Point(284, 165);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(125, 38);
+            this.txtQuantity.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblTaxRate);
+            this.Controls.Add(this.cmbTaxRate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbTaxRate;
+        private System.Windows.Forms.Label lblTaxRate;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+    }
+}
+
---
+++ Week2TaxRateGUI/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week2TaxRateGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            const double OVER_BUDGET_AMOUNT = 100;
+            const double five_percent = .05;
+            const double six_percent = .06;
+            const double seven_percent = .07;
+            const double eight_percent = .08;
+            const double nine_percent = .09;
+
+            int quantity = 0;
+            double item_price = 0;
+            double total_price = 0;
+
+            quantity = Convert.ToInt32(txtQuantity.Text);
+            item_price = Convert.ToDouble(txtPrice.Text);
+
+            total_price = item_price * quantity;
+
+            
+
+            
+        }
+    }
+}
